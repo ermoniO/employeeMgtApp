c89e171b19eb39c6d1a3ae68c47797196861e453
Ermoni Osborne <moni.osborne@yahoo.com>
2022-05-25 16:59:38 -0400

    Add remaining

--- springboot-backend /src/main/java/net/javaguides/springbootbackend/repository/EmployeeRepository.java
+++ springboot-backend /src/main/java/net/javaguides/springbootbackend/repository/EmployeeRepository.java
@@ -1,12 +1,17 @@
 package net.javaguides.springbootbackend.repository;
 
-
+//Repository interface
 import org.springframework.data.jpa.repository.JpaRepository;
 import net.javaguides.springbootbackend.model.Employee;
 import org.springframework.stereotype.Repository;
 
+
+//JpaRepository exposes database call methods(Save, FindById, DeleteById, pagination, etc) Provides automatically for all of
+//the methods so we do not need to write any of it.
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
+R
 
 }
